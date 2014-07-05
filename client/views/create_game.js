13d476a0a07acb9276d8cfbad300a7d34529fbdd
Razvan Teslaru <razvan.teslaru@gmail.com>
2014-07-05 21:32:59 +0200

    removed async HTTP test

--- client/views/create_game.js
+++
@@ -1,9 +0,0 @@
-Template.createGame.getTest = function() {
-    resultArray = [];
-    HTTP.get('http://localhost:3000/wcards.txt', function(error, result) {
-        if (!error) {
-            console.log(result);
-            resultArray = result.content.split('<>');
-        }
-    });
-};
